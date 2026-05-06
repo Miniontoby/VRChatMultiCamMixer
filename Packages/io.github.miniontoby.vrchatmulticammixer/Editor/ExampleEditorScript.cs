500e4f1520045d4d844d229c5b3783865e2eccad
Miniontoby <47940064+Miniontoby@users.noreply.github.com>
2026-05-06 19:28:59 +0200

    Initial project setup/files

---
+++ Packages/io.github.miniontoby.vrchatmulticammixer/Editor/ExampleEditorScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace io.github.miniontoby.vrchatmulticammixer
+{
+	public class ExampleEditorScript
+	{
+		[MenuItem("VRMCPM/Control Panel")]
+		static void ControlPanel()
+		{
+			EditorUtility.DisplayDialog("Example Script", "Opened This Dialog", "OK");
+		}
+	}
+}
---
+++ Packages/io.github.miniontoby.vrchatmulticammixer/README.md
@@ -0,0 +1,4 @@
+# io.github.miniontoby.vrchatmulticammixer by Miniontoby
+
+A drag & drop multi-camera management system for VRChat worlds, allowing to do in-game video switching between multiple camera's to make multi cam livestreams easier to manage.
+
