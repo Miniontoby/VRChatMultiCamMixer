0c7c095a403fa9587cc91141837c910c462643ed
classyham <hamish@classyham.com>
2026-06-08 00:31:43 +0100

    CCreated tripod manager script + updated prefab

---
+++ Packages/io.github.miniontoby.vrchatmulticammixer/Runtime/Scripts/TripodController.cs
@@ -0,0 +1,114 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+public class TripodController : UdonSharpBehaviour
+{
+    [Header("References")]
+    [Tooltip("The pickup component from this object")]
+    [SerializeField] private VRC_Pickup pickupHandTarget; 
+    
+    [Tooltip("The transform that will be rotated to point at the pickup. This is usually the camera pivot on the tripod")]
+    [SerializeField] private Transform cameraPivot;   
+
+    [Tooltip("The transform that will be rotated")]
+    [SerializeField] private Transform gripResetPoint;    
+
+    [Header("Settings")]
+    [Tooltip("Smoothing factor for syncing to prevent jitter")]
+    [SerializeField] private float lerpSpeed = 15f;       
+
+    [Tooltip("Rotation offset if needed")]
+    [SerializeField] private Vector3 rotationOffsetEuler = new Vector3(0, -90, 0); //90 by default because it's sideways otherwise
+
+    //Networked Rotation
+    [UdonSynced] private Quaternion syncedRotation;
+
+    private bool isHeld = false;
+    private Rigidbody pickupRigidbody;
+    private Quaternion rotationCorrection;
+
+    void Start()
+    {
+        if (cameraPivot == null) cameraPivot = transform;
+        syncedRotation = cameraPivot.rotation;
+
+        if (pickupHandTarget != null)
+        {
+            pickupRigidbody = pickupHandTarget.GetComponent<Rigidbody>();
+        }
+
+        rotationCorrection = Quaternion.Euler(rotationOffsetEuler);
+    }
+
+    void LateUpdate()
+    {
+        if (Networking.IsOwner(gameObject))
+        {
+            if (isHeld && pickupHandTarget != null)
+            {
+                Vector3 targetDirection = pickupHandTarget.transform.position - cameraPivot.position;
+
+                if (targetDirection != Vector3.zero)
+                {
+                    Quaternion rawLookRotation = Quaternion.LookRotation(targetDirection);
+                    
+                    Quaternion targetRotation = rawLookRotation * rotationCorrection;
+                    
+                    cameraPivot.rotation = targetRotation;
+
+                    syncedRotation = targetRotation;
+                    RequestSerialization();
+                }
+            }
+            else if (!isHeld && gripResetPoint != null && pickupHandTarget != null)
+            {
+                pickupHandTarget.transform.position = gripResetPoint.position;
+                pickupHandTarget.transform.rotation = gripResetPoint.rotation;
+            }
+        }
+        else
+        {
+            cameraPivot.rotation = Quaternion.Slerp(cameraPivot.rotation, syncedRotation, Time.deltaTime * lerpSpeed);
+            
+            if (!isHeld && gripResetPoint != null && pickupHandTarget != null)
+            {
+                pickupHandTarget.transform.position = gripResetPoint.position;
+                pickupHandTarget.transform.rotation = gripResetPoint.rotation;
+            }
+        }
+    }
+
+    public override void OnPickup()
+    {
+        if (!Networking.IsOwner(gameObject))
+        {
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        }
+        isHeld = true;
+    }
+
+    public override void OnDrop()
+    {
+        isHeld = false;
+
+        if (pickupHandTarget != null && gripResetPoint != null)
+        {
+            if (!Networking.IsOwner(pickupHandTarget.gameObject))
+            {
+                Networking.SetOwner(Networking.LocalPlayer, pickupHandTarget.gameObject);
+            }
+            pickupHandTarget.transform.position = gripResetPoint.position;
+            pickupHandTarget.transform.rotation = gripResetPoint.rotation;
+
+            if (pickupRigidbody != null)
+            {
+                pickupRigidbody.velocity = Vector3.zero;
+                pickupRigidbody.angularVelocity = Vector3.zero;
+            }
+        }
+    }
+
+}
